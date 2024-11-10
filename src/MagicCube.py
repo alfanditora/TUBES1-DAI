3348540bf2cab72dfdacc11d24332fec180d2cde
alfanditora <ditoraisakbar@gmail.com>
2024-11-11 01:29:22 +0700

    Fixing

--- src/MagicCube.py
+++ src/MagicCube.py
@@ -8,11 +8,11 @@ class MagicCube(object):
         else:
             self.size = 5
             self.cube = cube
-        self.magic_number = 315                # sum of each line should be 315
-        self.value = self.calculate_value()    # Store the calculated value as an attribute
+        self.magic_number = 315
+        self.value = self.calculate_value()
 
     def create_random_cube(self):
-        numbers = list(range(1, 126))  # 1 to 125 for 5x5x5 cube
+        numbers = list(range(1, 126))
         random.shuffle(numbers)
         
         cube = []
@@ -33,44 +33,42 @@ class MagicCube(object):
             cube = self.cube
         value = 0
         
-        # Check rows (75 lines)
+        # (75 lines)
         for i in range(self.size):
             for j in range(self.size):
-                # rows in xy-plane
+                # xy-plane
                 if sum(cube[i][j]) == self.magic_number:
                     value += 1
-                # rows in xz-plane
+                # xz-plane
                 if sum(cube[i][k][j] for k in range(self.size)) == self.magic_number:
                     value += 1
-                # rows in yz-plane
+                # yz-plane
                 if sum(cube[k][i][j] for k in range(self.size)) == self.magic_number:
                     value += 1
 
-        # Check diagonals in each 2D plane (15 planes * 2 diagonals = 30 lines)
+        # Diagonal bidang (30 lines)
         for i in range(self.size):
-            # diagonals in xy-plane
+            # diagonal xy-plane
             if sum(cube[i][j][j] for j in range(self.size)) == self.magic_number:
                 value += 1
             if sum(cube[i][j][self.size-1-j] for j in range(self.size)) == self.magic_number:
                 value += 1
             
-            # diagonals in xz-plane
+            # diagonal xz-plane
             if sum(cube[i][j][j] for j in range(self.size)) == self.magic_number:
                 value += 1
             if sum(cube[i][self.size-1-j][j] for j in range(self.size)) == self.magic_number:
                 value += 1
             
-            # diagonals in yz-plane
+            # diagonal yz-plane
             if sum(cube[j][i][j] for j in range(self.size)) == self.magic_number:
                 value += 1
             if sum(cube[j][i][self.size-1-j] for j in range(self.size)) == self.magic_number:
                 value += 1
 
-        # Check space diagonals (4 lines)
-        # Main space diagonal
+        # Diagonal ruang (4 lines)
         if sum(cube[i][i][i] for i in range(self.size)) == self.magic_number:
             value += 1
-        # Other space diagonals
         if sum(cube[i][i][self.size-1-i] for i in range(self.size)) == self.magic_number:
             value += 1
         if sum(cube[i][self.size-1-i][i] for i in range(self.size)) == self.magic_number:
@@ -94,7 +92,6 @@ class MagicCube(object):
 
     def get_successor(self, mode="random"):
         if mode == "random":
-            # Get two random positions
             pos1 = (random.randint(0, 4), random.randint(0, 4), random.randint(0, 4))
             pos2 = (random.randint(0, 4), random.randint(0, 4), random.randint(0, 4))
             while pos1 == pos2:
@@ -104,7 +101,7 @@ class MagicCube(object):
             return MagicCube(new_cube)
         
         elif mode == "best":
-            current_value = self.value  # Use the stored value
+            current_value = self.value
             best_value = current_value
             best_cube = self.copy_cube(self.cube)
             # best_pos1 = None
@@ -117,7 +114,6 @@ class MagicCube(object):
                         for i2 in range(self.size):
                             for j2 in range(self.size):
                                 for k2 in range(self.size):
-                                    # Skip if same position
                                     if (i1, j1, k1) >= (i2, j2, k2):
                                         continue
                                     
@@ -144,7 +140,7 @@ class MagicCube(object):
             for row in self.cube[i]:
                 print([f"{x:3d}" for x in row])
         print(f"\nCurrent Value: {self.value}")
-        if self.value == 109:  # Total possible lines that should sum to 315
+        if self.value == 109:
             print("Congratulations! Magic cube solved!")
         else:
             print("Magic cube not yet solved.")
